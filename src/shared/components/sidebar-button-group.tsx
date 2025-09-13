f09d835e6e882f83caecc07692c55c61ae96767c
Ryan Elliott <rselliott99@gmail.com>
2025-09-12 23:52:13 -0400

    Refactor SidebarButtonGroup to use cn for button styles

--- src/shared/components/sidebar-button-group.tsx
+++ src/shared/components/sidebar-button-group.tsx
@@ -4,30 +4,26 @@ import { SidebarTrigger, useSidebar } from '@/components/ui/sidebar';
 import { Button } from '@/components/ui/button';
 import { Plus, Search } from 'lucide-react';
 import { useMemo } from 'react';
+import { ClassValue } from 'clsx';
+import { cn } from '../lib/ui-utils';
 
 export default function SidebarButtonGroup() {
   const { state, isMobile, openMobile } = useSidebar();
 
   const isExpanded = useMemo(() => (isMobile ? openMobile : state === 'expanded'), [state, openMobile, isMobile]);
 
+  const buttonStyles: ClassValue = {
+    'h-0 w-0 opacity-0': isExpanded,
+  };
+
   return (
-    <div className="fixed top-2 left-4 z-40 flex items-center rounded-md bg-sidebar p-1">
+    <div className="group/sidebar-button-group fixed top-2 left-2 z-40 flex items-center rounded-md bg-sidebar p-1">
       <SidebarTrigger className="rounded-sm" />
 
-      <Button
-        variant="ghost"
-        size="icon"
-        className="size-7 rounded-sm duration-150 ease-in-out data-[expanded=true]:h-0 data-[expanded=true]:w-0 data-[expanded=true]:opacity-0"
-        data-expanded={isExpanded}
-      >
+      <Button variant="ghost" size="icon" className={cn('size-7 rounded-sm duration-150 ease-in-out', buttonStyles)}>
         <Search />
       </Button>
-      <Button
-        variant="ghost"
-        size="icon"
-        className="size-7 rounded-sm duration-150 ease-in-out data-[expanded=true]:h-0 data-[expanded=true]:w-0 data-[expanded=true]:opacity-0"
-        data-expanded={isExpanded}
-      >
+      <Button variant="ghost" size="icon" className={cn('size-7 rounded-sm duration-150 ease-in-out', buttonStyles)}>
         <Plus />
       </Button>
     </div>
